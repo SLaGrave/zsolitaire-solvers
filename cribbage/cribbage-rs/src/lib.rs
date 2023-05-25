3000360557848905cd7fd0ee270f2a52ab4eba43
Sam LaGrave <srl0011@uah.edu>
2023-05-24 19:35:19 -0500

    Random stuff

--- cribbage/cribbage-rs/src/lib.rs
+++ cribbage/cribbage-rs/src/lib.rs
@@ -1,5 +1,62 @@
+extern crate libc;
+
+use libc::c_char;
+use std::ffi::CStr;
 
 #[no_mangle]
-pub extern "C" fn addition(a: u32, b: u32) -> u32 {
-    a + b
+#[warn(improper_ctypes_definitions)]  // String not FFI safe
+pub extern "C" fn custom_print(s: String, b: bool) {
+    if b {
+        println!("From Rust: {}", s);
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn run_simulation(
+    col_a: *const c_char,
+    col_b: *const c_char,
+    col_c: *const c_char,
+    col_d: *const c_char
+) -> bool {
+
+    let a = unsafe {
+        CStr::from_ptr(col_a)
+    }.to_str().unwrap();
+    let b = unsafe {
+        CStr::from_ptr(col_b)
+    }.to_str().unwrap();
+    let c = unsafe {
+        CStr::from_ptr(col_c)
+    }.to_str().unwrap();
+    let d = unsafe {
+        CStr::from_ptr(col_d)
+    }.to_str().unwrap();
+
+    let a_vec = make_vec(a.to_string());
+    let b_vec = make_vec(b.to_string());
+    let c_vec = make_vec(c.to_string());
+    let d_vec = make_vec(d.to_string());
+
+    
+
+    true
+}
+
+fn calc(a: Vec<u32>, b: Vec<u32>, c: Vec<u32>, d: Vec<u32>) -> (i32, String) {
+    (0, "test".to_string())
+}
+
+fn make_vec(s: String) -> Vec<u32> {
+    let mut v = vec!();
+    for c in s.chars() {
+        let i = match c {
+            'A' => 1,
+            'J' => 10,
+            'Q' => 10,
+            'K' => 10,
+            _ => c.to_digit(10).unwrap()
+        };
+        v.push(i);
+    }
+    v
 }
--- cribbage/cribbage.cpp
+++ cribbage/cribbage.cpp
@@ -1,10 +1,38 @@
+#include <iostream>
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
 
 #include "./cribbage-rs/cribbage-rs.h"
 
+using namespace std;
+
+///////////////////////////////////////////
+// Config consts
+///////////////////////////////////////////
+
+///////////////////////////////////////////
+// Main function
+///////////////////////////////////////////
 int main(void) {
-  uint32_t sum = addition(1, 2);
-  printf("%" PRIu32 "\n", sum);
+  char colA[14], colB[14], colC[14], colD[14];
+
+  cout << "Please enter the values of column A:" << endl;
+  cin >> colA;
+  cout << "\tColumn A contains: " << colA << endl;
+
+  cout << "Please enter the values of column B:" << endl;
+  cin >> colB;
+  cout << "\tColumn B contains: " << colB << endl;
+
+  cout << "Please enter the values of column C:" << endl;
+  cin >> colC;
+  cout << "\tColumn C contains: " << colC << endl;
+
+  cout << "Please enter the values of column D:" << endl;
+  cin >> colD;
+  cout << "\tColumn D contains: " << colD << endl;
+
+  bool success = run_simulation(colA, colB, colC, colD);
+  cout << success << endl;
 }
